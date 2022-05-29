502ad438983fe58e2abc0fa67aa16f36e8c3c655
samxxxryu <ryu-samurai0930@outlook.jp>
2022-05-29 16:11:18 +0900

    カリキュラム8-5

--- app/Post.php
+++ app/Post.php
@@ -3,12 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    public function getPaginateByLimit(int $limit_count = 10)
+    protected $fillable = [
+    'title',
+    'body',
+    ];
+    public function getPaginate()
     {
-        // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this->orderBy('updated_at','DESC')->paginate(5);
     }
 }
